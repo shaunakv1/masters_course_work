64c24591b7dbda661c3707eb88f7071ba4121b2a
shaunakv1 <shaunakv1@85963d5a-eea2-47ee-89b6-e3ff71fd2f6a>
2011-05-09 11:25:08 +0000

    adapted to command response mode and corrected the parser

--- trunk/CourseWork/cps/ardunio/app/jobs/collect_data.rb
+++ trunk/CourseWork/cps/ardunio/app/jobs/collect_data.rb
@@ -1,41 +1,75 @@
 require 'reading'
 require 'serialport'
 
-#params for serial port
-port_str = "COM4"  #may be different for you
-baud_rate = 57600
-data_bits = 8
-stop_bits = 1
-parity = SerialPort::NONE
+ #params for serial port
+  port_str = "COM3"  #may be different for you
+  baud_rate = 57600
+  data_bits = 8
+  stop_bits = 1
+  parity = SerialPort::NONE
 
-sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
+  @@sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
 
-puts "Starting.."
-#just read forever
-while true do
-  reading = sp.read
-  reading.strip!
- # puts "Raw:#{reading}|"
-
-  if !reading.empty?
-  	params = reading.split("|")
-  	puts Time.now
-  	puts "Temperature = #{params[0]}"
-  	puts "Moisture = #{params[1]}"
-  	puts "adding to db.."
-
-  	reading = Reading.new
-	reading.station_id = 1
-	reading.temperature = params[0]
-	reading.moisture = params[1]
-	reading.created_at = Time.now + 5.hours
-	reading.save
-	puts "done.."
-	puts "------------------------"
+def read_sp
+  reading = ""
+  check_time = Time.now+10.seconds
+  while true do
+    reading = @@sp.read
+    
+    if !reading.empty? #stop if input is received on serial port
+      break
+    end
+    
+    if Time.now >= check_time #stop reading after 20 seconds
+      puts "no data received for the command.."
+      break
+    end    
+  
   end
-  sleep 1.0
+  
+  reading.strip!
+  reading
 end
 
-sp.close
+def start_collecting
+ 
+  #$1#22#141@
+  puts "Starting.."
+  while true do
+      
+    @@sp.write "$#2#temp#moist@"
+    sleep 1.0
+    reading = read_sp
+    puts "Raw:#{reading}"
 
+    if !reading.empty?
+    	reading.chop!
+      reading.slice!(0)
+      reading.slice!(0)
+      params = reading.split("#")
+      #puts "processed: 1:#{params[0]} 2:#{params[1]} 3: #{params[2]}"
+      
+    	puts Time.now
+      puts "ID = #{params[0]}"    
+    	puts "Temperature = #{params[1]}"
+    	puts "Moisture = #{params[2]}"
+    	puts "adding to db.."
+
+      reading = Reading.new
+    	reading.station_id = params[0]
+    	reading.temperature = params[1]
+    	reading.moisture = params[2]
+    	reading.created_at = Time.now + 5.hours
+    	reading.save
+    	puts "done.."
+    	puts "------------------------"
+     end
+    sleep 5.0
+  end
+
+  @@sp.close
+  puts "Stopping.."
+end
+
+start_collecting
 
