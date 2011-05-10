18293724789fc9039677434e13ca4166160099ea
shaunakv1 <shaunakv1@85963d5a-eea2-47ee-89b6-e3ff71fd2f6a>
2011-05-10 03:05:04 +0000

    Almost final code. Snaps added

--- cps/ardunio/app/jobs/dbinfo.rb
+++ cps/ardunio/app/jobs/dbinfo.rb
@@ -1,4 +1,5 @@
 ActiveRecord::Base.establish_connection(
   :adapter => 'sqlite3',
-  :database => '../../db/development.sqlite3'
+  :database => '../../db/development.sqlite3',
+  :timeout => 10000
 )
